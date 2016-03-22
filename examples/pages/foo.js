0908264afb36d6ff4bd62ae24e16fd31f1100295
derekja <derekja@gmail.com>
2016-03-22 15:18:03 -0700

    changes to check a component

---
+++ examples/pages/foo.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+
+class DemoComponent extends Component {
+
+  render() {
+    return (
+      <div className="Demo">
+        <h2>Demo</h2>
+        <p>Non-information goes here.</p>
+      </div>
+    )
+  }
+}
+
+export default DemoComponent;
+
